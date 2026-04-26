fb57c16b323dedc29f4e26d3957df8e16ad156a3
smit-dudhatra <141557682+smit-dudhatra@users.noreply.github.com>
2026-04-26 11:33:43 +0530

    Create vps-scripts-prerequistis.py

---
+++ vps-scripts-prerequistis.py
@@ -0,0 +1,5 @@
+check if all dependencies are installed correctly or not
+IST time is applied in the script or not ?
+simulated everyday scenario with the script already ?
+  give exception cases to solve
+    is outcome desired ?
