29c7a37a59a42991ce2aa7102b8d507b655d4149
smit-dudhatra <141557682+smit-dudhatra@users.noreply.github.com>
2026-04-27 21:35:21 +0530

    Add conditions for trading scenarios in script

--- vps-scripts-prerequistis.py
+++ vps-scripts-prerequistis.py
@@ -3,3 +3,10 @@ IST time is applied in the script or not ?
 simulated everyday scenario with the script already ?
   give exception cases to solve
     is outcome desired ?
+
+badha ma individually
+  exchange == "NFO" , "MCX"
+  ends with "CE" or "PE"
+  starts with "NIFTY"
+
+  kite na orders and postions ma badhe j apply karo
